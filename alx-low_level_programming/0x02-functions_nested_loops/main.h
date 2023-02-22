ca30ecab50ed3a960f0ce6edd54639b7ab5a0c3d
Jimmy <you@example.com>
2023-02-22 08:30:29 -0800

    update

---
+++ alx-low_level_programming/0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#infdef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _inslower(int c);
+int _isalpha(int c);
+int print_sign(int c);
+int _abs(int);
+int print_last_digit(int);
+int jack_bauer(void);
+int times_table(void);
+int add(int,int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
